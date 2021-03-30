2c5eb7b80883767944295a2ec028f134a732ed78
Angela Gilhotra <angelagilhotra@gmail.com>
2021-03-30 20:39:57 +0530

    29/3

--- src/views/Gift.vue
+++ src/views/Gift.vue
@@ -13,122 +13,127 @@
           class="relative px-1 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20"
         >
           <div class="max-w-md mx-auto">
-            <div v-if="fielding.ok">
-              <p class="font-heading text-5xl">
-                {{ fromApi.name }}
-              </p>
-              <div>
-                token id:
-                <span
-                  class="font-mono text-xs border-dashed border-b-2 border-gray-600 cursor-pointer"
-                  @click="$store.commit('togglePanel', true)"
-                >
-                  {{ fromApi.token }}
-                </span>
-              </div>
-              <div>
-                network:
-                <span class="font-mono text-xs">
-                  {{ network }}
-                </span>
-              </div>
-              <!-- <p class="font-heading text-5xl">
+            <div v-if="!loading">
+              <div v-if="fielding.ok">
+                <p class="font-heading text-5xl">
+                  {{ fromApi.name }}
+                </p>
+                <div>
+                  token id:
+                  <span
+                    class="font-mono text-xs border-dashed border-b-2 border-gray-600 cursor-pointer"
+                    @click="$store.commit('togglePanel', true)"
+                  >
+                    {{ fromApi.token }}
+                  </span>
+                </div>
+                <div>
+                  network:
+                  <span class="font-mono text-xs">
+                    {{ network }}
+                  </span>
+                </div>
+                <!-- <p class="font-heading text-5xl">
               Hello!
             </p> -->
-              <!-- <p class="truncate font-mono text-xs">
+                <!-- <p class="truncate font-mono text-xs">
               {{ hash }}
             </p> -->
-              <!-- <p>loading: {{ fielding.loading }}</p> -->
-              <div>
-                <!-- <p>claim: {{ fromApi.claim_status }}</p> -->
-                <!-- <p>proof: {{ fromApi.proof }}</p> -->
-                <p v-if="Object.keys(fromApi.award).length > 0">
-                  award: {{ fromApi.award }}
-                </p>
-                <!-- <p v-if="fromApi.gratitude">gratitude: {{ fromApi.gratitude }}</p> -->
-                <!-- <p>image: {{ fromApi.image }}</p> -->
-                <!-- insert image here -->
-                <!-- <div id="canvas"></div> -->
-                <div class="my-5">
-                  <Canvas
-                    :fellowName="fromApi.name"
-                    class="h-96 w-96 shadow-2xl"
-                  />
-                </div>
-              </div>
-              <div v-if="!loading">
-                <!-- if claim status = false / not already minted -->
-                <div v-if="!$store.getters.claim_status">
-                  <!-- display if metamask is not connected -->
-                  <div v-if="!$store.getters.account">
-                    <button
-                      class="bg-purple-500 hover:bg-purple-900 text-white font-bold py-3 px-4 rounded-lg shadow-2xl font-kernel"
-                      v-on:click="connectMetamask"
-                    >
-                      Connect Metamask
-                    </button>
-                    <!-- <p>account: {{ $store.getters.account }}</p>
-                    <p>network id: {{ $store.getters.network }}</p> -->
+                <!-- <p>loading: {{ fielding.loading }}</p> -->
+                <div>
+                  <!-- <p>claim: {{ fromApi.claim_status }}</p> -->
+                  <!-- <p>proof: {{ fromApi.proof }}</p> -->
+                  <p v-if="Object.keys(fromApi.award).length > 0">
+                    award: {{ fromApi.award }}
+                  </p>
+                  <!-- <p v-if="fromApi.gratitude">gratitude: {{ fromApi.gratitude }}</p> -->
+                  <!-- <p>image: {{ fromApi.image }}</p> -->
+                  <!-- insert image here -->
+                  <!-- <div id="canvas"></div> -->
+                  <div class="my-5">
+                    <Canvas
+                      :fellowName="fromApi.name"
+                      class="h-96 w-96 shadow-2xl"
+                    />
                   </div>
-                  <!-- display if metamask is connected -->
-                  <div v-else>
-                    <!-- display if the metamask is connected to the correct network -->
-                    <button
-                      class="bg-purple-500 hover:bg-purple-900 text-white font-bold py-3 px-4 rounded-lg shadow-2xl font-kernel"
-                      v-on:click="claim"
-                      v-if="$store.getters.network == $store.getters.chainId"
-                    >
-                      Claim
-                    </button>
-
-                    <!-- display if the metamask is not connected to the correct network -->
+                </div>
+                <div v-if="!loading">
+                  <!-- if claim status = false / not already minted -->
+                  <div v-if="!$store.getters.claim_status">
+                    <!-- display if metamask is not connected -->
+                    <div v-if="!$store.getters.account">
+                      <button
+                        class="bg-purple-500 hover:bg-purple-900 text-white font-bold py-3 px-4 rounded-lg shadow-2xl font-kernel"
+                        v-on:click="connectMetamask"
+                      >
+                        Connect Metamask
+                      </button>
+                      <!-- <p>account: {{ $store.getters.account }}</p>
+                    <p>network id: {{ $store.getters.network }}</p> -->
+                    </div>
+                    <!-- display if metamask is connected -->
                     <div v-else>
-                      <p>
-                        <!-- Please connect to chain id
+                      <!-- display if the metamask is connected to the correct network -->
+                      <button
+                        class="bg-purple-500 hover:bg-purple-900 text-white font-bold py-3 px-4 rounded-lg shadow-2xl font-kernel"
+                        v-on:click="claim"
+                        v-if="$store.getters.network == $store.getters.chainId"
+                      >
+                        Claim
+                      </button>
+
+                      <!-- display if the metamask is not connected to the correct network -->
+                      <div v-else>
+                        <p>
+                          <!-- Please connect to chain id
                         {{ $store.getters.chainId }} & reload -->
-                        Please connect to {{ network }} and reload
-                      </p>
+                          Please connect to {{ network }} and reload
+                        </p>
+                      </div>
                     </div>
                   </div>
+                  <!-- if already minted -->
+                  <div v-else>
+                    <p class="truncate">
+                      The token is minted to:
+                      <span class="font-mono truncate">
+                        <a :href="linkTo.tokens" target="_new">
+                          {{ $store.getters.currentOwner }}
+                        </a>
+                      </span>
+                    </p>
+                  </div>
                 </div>
-                <!-- if already minted -->
                 <div v-else>
-                  <p class="truncate">
-                    The token is minted to:
-                    <span class="font-mono truncate">
-                      <a :href="linkTo.tokens" target="_new">
-                        {{ $store.getters.currentOwner }}
-                      </a>
-                    </span>
-                  </p>
+                  <div class="spinner"></div>
                 </div>
               </div>
+
               <div v-else>
-                <div class="spinner"></div>
+                <p class="py-3 text-2xl">{{ fielding.notAuthMsg }}</p>
+              </div>
+
+              <div v-if="fielding.msg.txHash" class="pt-4">
+                <p>
+                  Minting transaction sent
+                </p>
+                <p class="truncate">
+                  Transaction hash:
+                  <span class="font-mono truncate">
+                    <a :href="linkTo.txHash" target="_new">
+                      {{ fielding.msg.txHash }}
+                    </a>
+                  </span>
+                </p>
+              </div>
+              <div v-if="fielding.msg.alert" class="pt-4">
+                <p>
+                  {{ fielding.msg.alert }}
+                </p>
               </div>
             </div>
-
             <div v-else>
-              <p class="py-3 text-2xl">{{ fielding.notAuthMsg }}</p>
-            </div>
-
-            <div v-if="fielding.msg.txHash" class="pt-4">
-              <p>
-                Minting transaction sent
-              </p>
-              <p class="truncate">
-                Transaction hash:
-                <span class="font-mono truncate">
-                  <a :href="linkTo.txHash" target="_new">
-                    {{ fielding.msg.txHash }}
-                  </a>
-                </span>
-              </p>
-            </div>
-            <div v-if="fielding.msg.alert" class="pt-4">
-              <p>
-                {{ fielding.msg.alert }}
-              </p>
+              <div class="spinner"></div>
             </div>
           </div>
         </div>
@@ -191,14 +196,14 @@ export default {
     });
   },
   created: async function() {
-    this.$data.loading = false;
+    this.$data.loading = true;
     // fetch hash details
     let r = await axios.get(
       "https://testing-gift-api.herokuapp.com/hash/" + this.$props.hash
     );
 
     // details fetched -> set loading to false
-    this.$data.fielding.loading = false;
+    this.$data.loading = false;
     const { ok } = r.data;
 
     // pick and display info
